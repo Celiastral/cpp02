419a6d021810e6b383c84b3d8fc0dd23c21c4e00
Celia <elioandre692@gmail.com>
2024-09-15 18:49:39 +0200

    fix: fixed the point being not inside the triangle is said point is on the triangle side

--- ex03/srcs/bsp.cpp
+++ ex03/srcs/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: eandre <eandre@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 23:30:08 by eandre            #+#    #+#             */
-/*   Updated: 2024/09/15 18:24:25 by eandre           ###   ########.fr       */
+/*   Updated: 2024/09/15 18:49:00 by eandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,28 @@ Fixed area(Point const a, Point const b, Point const c)
 	return (area * Fixed(0.5f));
 }
 
+Fixed crossProduct(Point a, Point b)
+{
+    return (a.get_x() * b.get_y() - a.get_y() * b.get_x());
+}
+
+bool isPointOnTriangleSide(Point p1, Point p2, Point p3, Point p)
+{
+	if ((p.get_x() == p1.get_x() && p.get_y() == p1.get_y())
+	|| (p.get_x() == p2.get_x() && p.get_y() == p2.get_y()) 
+	|| (p.get_x() == p3.get_x() && p.get_y() == p3.get_y()))
+        return true;
+
+    Fixed	cp1 = crossProduct(Point(Fixed(p1.get_y() - p.get_y()).toFloat(), Fixed(p1.get_x() - p.get_x()).toFloat())
+	, Point(Fixed(p2.get_y() - p.get_y()).toFloat(), Fixed(p2.get_x() - p.get_x()).toFloat()));
+    Fixed	cp2 = crossProduct(Point(Fixed(p2.get_y() - p.get_y()).toFloat(), Fixed(p2.get_x() - p.get_x()).toFloat())
+	, Point(Fixed(p3.get_y() - p.get_y()).toFloat(), Fixed(p3.get_x() - p.get_x()).toFloat()));
+    Fixed	cp3 = crossProduct(Point(Fixed(p3.get_y() - p.get_y()).toFloat(), Fixed(p3.get_x() - p.get_x()).toFloat())
+	, Point(Fixed(p1.get_y() - p.get_y()).toFloat(), Fixed(p1.get_x() - p.get_x()).toFloat()));
+
+    return (cp1 == 0 && cp2 == 0 && cp3 == 0);
+}
+
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
 	Fixed	area_main_triangle;
@@ -36,5 +58,5 @@ bool bsp( Point const a, Point const b, Point const c, Point const point)
 	area_second_triangle = area(a, point, c);
 	area_third_triangle = area(a, b, point);
 	combined_area = area_first_triangle + area_second_triangle + area_third_triangle;
-	return (combined_area == area_main_triangle);
+	return (combined_area == area_main_triangle && !isPointOnTriangleSide(a, b, c, point));
 }
--- ex03/srcs/main.cpp
+++ ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eandre <eandre@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 17:07:09 by eandre            #+#    #+#             */
-/*   Updated: 2024/09/15 18:24:35 by eandre           ###   ########.fr       */
+/*   Updated: 2024/09/15 18:44:15 by eandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(void)
 	Point	b(10.0f, 10.0f);
 	Point	c(10.0f, 0.0f);
 	Point	Point_1(1.5f, 1.1f);
-	Point	Point_2(10.5f, 10.5f);
+	Point	Point_2(10.0f, 10.0f);
 
 	if (bsp(a, b, c, Point_1) == true)
 		std::cout << "\033[0;32mThe point is inside the triangle ! :>\033[0m" << std::endl;
