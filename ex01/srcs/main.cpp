5849dd3dec929f0b7b4c1f1dc97ad57c181314e0
Celia <elioandre692@gmail.com>
2024-09-15 17:34:16 +0200

    fashion: added some tests to ex01

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eandre <eandre@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 17:07:09 by eandre            #+#    #+#             */
-/*   Updated: 2024/09/10 20:24:16 by eandre           ###   ########.fr       */
+/*   Updated: 2024/09/15 17:33:35 by eandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,17 @@ int	main(void)
 	Fixed const c( 42.42f );
 	Fixed const d( b );
 	a = Fixed( 1234.4321f );
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a << " as fixed point" << std::endl;
+	std::cout << "a is " << a.toFloat() << " as floating point" << std::endl;
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b << " as fixed point" << std::endl;
+	std::cout << "b is " << b.toFloat() << " as floating point" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c << " as fixed point" << std::endl;
+	std::cout << "c is " << c.toFloat() << " as floating point" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d << " as fixed point" << std::endl;
+	std::cout << "d is " << d.toFloat() << " as floating point" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 
 }
