84fcee50dea16c10af49199a18539ab8e03aab18
Celia <elioandre692@gmail.com>
2024-09-15 18:16:11 +0200

    fix: fixed the multiplication return value being wrong, added some tests in the main

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eandre <eandre@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 17:07:09 by eandre            #+#    #+#             */
-/*   Updated: 2024/09/10 22:57:27 by eandre           ###   ########.fr       */
+/*   Updated: 2024/09/15 18:15:18 by eandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,20 @@ int	main(void)
 {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed c(1);
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
+	std::cout << c << std::endl;
+	if (a < c)
+		std::cout << "c is greater than a" << std::endl;
+	else
+		std::cout << "a is greater than c" << std::endl;
 	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
+	std::cout << a-- << std::endl;
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 	std::cout << Fixed::max(a, b) << std::endl;
+	std::cout << b << std::endl;
+	std::cout << b/Fixed(2) << std::endl;
+	std::cout << b + c << std::endl;
 }
